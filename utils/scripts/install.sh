b2eb2cadffcf30176bbe8fb776867b6a87d7dfca
Herpiko Dwi Aguno <herpiko@gmail.com>
2019-04-03 23:10:08 +0700

    Add init.d scripts for daemon. Move miscellaneous files to utils.

---
+++ utils/scripts/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Download and extract
+sudo rm -f /tmp/irgsh-go.tar.gz && cd /tmp && wget https://github.com/BlankOn/irgsh-go/releases/download/v0.0.9-alpha/release.tar.gz -O ./irgsh-go.tar.gz && tar -xvf irgsh-go.tar.gz 
+
+# Create required dirs
+sudo mkdir -p /etc/irgsh
+sudo mkdir -p /usr/share/irgsh
+sudo mkdir -p /var/irgsh/chief
+sudo mkdir -p /var/irgsh/builder
+sudo mkdir -p /var/irgsh/repo
+sudo mkdir -p /var/log/irgsh
+
+# Install the files
+
+# Binaries
+sudo cp -v /tmp/irgsh-go/usr/bin/* /usr/bin/
+# Templates
+sudo cp -vR /tmp/irgsh-go/usr/share/irgsh/* /usr/share/irgsh/
+# Configuration
+sudo cp -v /tmp/irgsh-go/etc/irgsh/config.yml /etc/irgsh/config.yml
+# Daemon
+sudo cp -v /tmp/irgsh-go/etc/irgsh/config.yml /etc/irgsh/config.yml
