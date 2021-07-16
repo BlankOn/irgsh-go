472ee2e191b037737b63d1f68af57d67d465fba4
Herpiko Dwi Aguno <herpiko@gmail.com>
2021-07-16 19:03:47 +0000

    #139 --ignore-checks should be only works in --experimental.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -190,7 +190,7 @@ func main() {
 			},
 			Action: func(ctx *cli.Context) (err error) {
 
-				ignoreChecks := ctx.Bool("ignore-checks")
+				ignoreChecks := ctx.Bool("ignore-checks") && ctx.Bool("experimental")
 
 				err = checkForInitValues()
 				if err != nil {
